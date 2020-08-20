b379918856d7f951bed55abfcb8c552b3a4f64d7
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-20 04:21:48 +0300

    add post form

--- awwwards/forms.py
+++ awwwards/forms.py
@@ -9,3 +9,15 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username","email","password1", "password2"]
+
+class PostForm(forms.ModelForm):
+    image = CloudinaryFileField(
+    options = { 
+      'tags': "directly_uploaded",
+      'crop': 'limit', 'width': 1000, 'height': 1000,
+      'eager': [{ 'crop': 'fill', 'width': 150, 'height': 100 }]
+    })
+
+    class Meta:
+        model = Project
+        fields = ('title', 'details', 'link', 'image')
