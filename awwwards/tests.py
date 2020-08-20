a7d614d4c6a6436670feca6e8be1f9f5d80f1c92
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-20 03:58:48 +0300

    add project test

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -15,3 +15,32 @@ class TestProfile(TestCase):
 
     def test_delete_user(self):
         self.user.delete()
+
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='cecilia')
+        self.post = Project.objects.create(id=1, title='test post', image='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', details='desc', user=self.user, link='http://ur.coml')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Project))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Project.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Project.fetch_all_images()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Project.search_project_by_title('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Project.search_project_by_title('test')
+        self.assertTrue(len(post) < 1)
+
